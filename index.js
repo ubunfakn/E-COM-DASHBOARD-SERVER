66292ab5ff2acf62d122295f7a22ce192cf66c91
ubunfakn <ankitnashine12@gmail.com>
2023-06-22 17:01:50 +0530

    Jwt Authenctication integrated

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 const connect = require("../E-DASHBOARD/MongoDB_Config/Config");
 const User = require("../E-DASHBOARD/MongoDB_Config/Users");
 const Product = require('../E-DASHBOARD/MongoDB_Config/Products');
+const Jwt = require('jsonwebtoken');
+const jwtKey = 'ecom'
 
 const app = express();
 
@@ -14,31 +16,52 @@ app.post("/register", async (req, resp) => {
   let data = await user.save();
   data = data.toObject();
   delete data.password;
-  resp.send(data);
+  Jwt.sign({data:data},jwtKey,(err,token)=>{
+    if(!err){
+      resp.send({data, token});
+    }else{
+      resp.send()
+    }
+  })
 });
 
 app.post("/login", async (req, resp) => {
   if (req.body.email && req.body.password) {
-    let user = await User.findOne(req.body).select("-password");
-    resp.send(user ? user : { err: true });
-  } else {
+      let user = await User.findOne(req.body).select("-password");
+      if(user)
+      {
+          Jwt.sign({user:user},jwtKey,(err,token)=>{
+            if(!err){
+              resp.send({user,token});
+            }else{
+              resp.send()
+            }
+          })
+      }
+      else
+      {
+          resp.send();
+      }
+  } 
+  else 
+  {
     resp.send(404);
   }
 });
 
-app.post("/getuser", async (req, resp)=>{
+app.post("/getuser", verifyToken, async (req, resp)=>{
   let user = await User.findOne(req.body).select("-password");
   resp.send(user ? user : {err: true});
 })
 
-app.post('/add_product',async (req, resp)=>{
+app.post('/add_product', verifyToken, async (req, resp)=>{
   let product = new Product(req.body);
   // console.log(req.body);
   let result = await product.save();
   resp.send(result);
 })
 
-app.get(`/get_products/:uId`, async (req,resp)=>{
+app.get(`/get_products/:uId`,verifyToken, async (req,resp)=>{
   let products= await Product.find({userId:req.params.uId});
   if(products.length>0){
     resp.send(products);
@@ -47,21 +70,21 @@ app.get(`/get_products/:uId`, async (req,resp)=>{
   }
 })
 
-app.get('/get_product/:id', async (req, resp)=>{
+app.get('/get_product/:id',verifyToken, async (req, resp)=>{
   const id = req.params.id;
   console.log(id)
   let product = await Product.findOne({_id:id});
   resp.send(product);
 })
 
-app.delete('/delete/:uId/:id', async (req,resp)=>{
+app.delete('/delete/:uId/:id',verifyToken, async (req,resp)=>{
   const id = req.params.id;
   console.log(id);
   const result = await Product.deleteOne({userId:req.params.uId,_id:id});
   resp.send(result);
 })
 
-app.put('/update/:id', async (req,resp)=>{
+app.put('/update/:id',verifyToken, async (req,resp)=>{
   const id = req.params.id;
   const data = req.body;
   console.log(data);
@@ -72,7 +95,7 @@ app.put('/update/:id', async (req,resp)=>{
   resp.send(result);
 })
 
-app.get('/search/:uId/:key', async (req, resp)=>{
+app.get('/search/:uId/:key',verifyToken, async (req, resp)=>{
   let result = await Product.find({
     "$or" :
     [
@@ -91,12 +114,29 @@ app.get('/search/:uId/:key', async (req, resp)=>{
   resp.send(result);
 })
 
-app.get('/getproduct/user_id/:uId', async (req,resp)=>{
+app.get('/getproduct/user_id/:uId',verifyToken, async (req,resp)=>{
   let id = req.params.uId
   console.log(id);
   let products = await Product.find({userId:id});
   if(products.length>0)resp.send(String(products.length));
   else resp.send(404);
 })
+
+function verifyToken(req, resp, next){
+  const token = req.headers['authorization'];
+  console.log(token);
+  if(token)
+  {
+    Jwt.verify(token,jwtKey,(err,success)=>{
+      if(err){
+        resp.status(401).send()
+      }else{
+        next();
+      }
+    })
+  }else{
+    resp.status(400).send();
+  }
+}
 
 app.listen(8080);
